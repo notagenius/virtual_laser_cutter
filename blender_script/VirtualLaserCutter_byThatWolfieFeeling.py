ab9f2db4f59a7ba3f9eb8efb2afeea8b05ed0919
ailen365 <shiplost@126.com>
2018-06-07 01:16:47 +0200

    script comments

--- blender_script/VirtualLaserCutter_byThatWolfieFeeling.py
+++ blender_script/VirtualLaserCutter_byThatWolfieFeeling.py
@@ -7,10 +7,12 @@ import bpy.ops
 import bmesh
 import mathutils
 
+#parameters
 filepath = "/doc/sample_Box_fill.svg"
 texturepath = "/texture/woody.jpg"
 thickness = 0.004
 
+#render engine and file import
 bpy.context.scene.render.engine = 'CYCLES'
 bpy.ops.import_curve.svg(filepath = filepath)
 
@@ -23,11 +25,11 @@ count = 0
 obj_count = 0
 obj_coordinate = mathutils.Vector((0,0,0))
 
-
 for mat in bpy.data.materials:
     if mat.name[0:10]=="Laser_Wood":
         count += 1
-        
+
+#location re-calculation for same layout along .svg        
 for obj_current in objList:
     obj_count += 1
     obj = bpy.data.objects[obj_current]
@@ -35,28 +37,31 @@ for obj_current in objList:
     bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
     obj_coordinate = obj_coordinate + obj.location
     
-
 obj_center = obj_coordinate/obj_count
 
 for obj_current in objList:
     string = "Laser_Wood"+ str(count)
     count += 1
     
+    #extruding with modifier
     obj = bpy.data.objects[obj_current]
     bpy.context.scene.objects.active=obj
     bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
     obj_new_location = (obj.location - obj_center)*10
     bpy.context.scene.objects.active.scale = (10,10,10)
     bpy.context.scene.objects.active.location = obj_new_location
+    #curve to mesh
     bpy.ops.object.convert(target='MESH') 
     bpy.ops.object.modifier_add(type='SOLIDIFY')
     bpy.context.object.modifiers["Solidify"].thickness = thickness
     
+    #uv map
     bpy.ops.object.mode_set(mode='EDIT')
     bpy.ops.mesh.select_all(action='SELECT') 
     bpy.ops.uv.unwrap()
     bpy.ops.object.mode_set(mode='OBJECT')
     
+    #material
     bpy.ops.object.material_slot_remove()
     bpy.ops.object.material_slot_add()
     laser_mat = bpy.data.materials.new(string)
@@ -68,6 +73,12 @@ for obj_current in objList:
     diffuse=bpy.data.materials[string].node_tree.nodes.new(type = 'ShaderNodeBsdfDiffuse')
     tex_image=bpy.data.materials[string].node_tree.nodes.new(type = 'ShaderNodeTexImage')
     bpy.data.materials[string].node_tree.links.new(tex_image.outputs['Color'], diffuse.inputs['Color'])
+    #texture
     tex_image.image=texture_image
     bpy.data.materials[string].node_tree.links.new(diffuse.outputs['BSDF'], output.inputs['Surface'])
     bpy.context.object.active_material = laser_mat
+
+#Todo (till June 2018)
+#develop into an extension with menu
+#engrave
+#texture with normal
